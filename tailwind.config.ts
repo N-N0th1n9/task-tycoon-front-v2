cd20afce8a8a97abb3d2c72b9db52ff719d4f73f
N-N0th1n9 <vladislav.tsiganov.04@gmail.com>
2025-01-30 21:16:19 +0300

    tailwind: move colors from config in globalcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,20 +4,7 @@ export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      colors: {
-        ttWhite: 'rgb(250, 250, 250)',
-
-        ttBlack: 'rgb(16, 18, 18)',
-        ttLightBlack: `rgb(51, 51, 51)`,
-
-        ttRed: 'rgb(255, 0, 0)',
-        ttLightRed: 'rgb(255, 225, 225)',
-
-        ttBlue: 'rgb(163, 255, 100)',
-
-        ttGray: 'rgb(130, 130, 130)',
-        ttLightGray: 'rgb(193, 193, 193)',
-      },
+      colors: {},
     },
   },
   plugins: [],
