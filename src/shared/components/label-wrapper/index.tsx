19e8acc34b76c620c9213348cd14c8f98d4bc4fa
sergeykochiev <sergeyivanovo547@gmail.com>
2025-02-02 20:53:12 +0300

    label-wrapper: added 'as' prop

--- src/shared/components/label-wrapper/index.tsx
+++ src/shared/components/label-wrapper/index.tsx
@@ -1,16 +1,19 @@
-import { FC, ReactNode } from 'react'
+import { ElementType, FC, ReactNode } from 'react'
 
-interface IProps {
+interface IProps<E extends ElementType = ElementType> {
+  as: E
   label: string
   children: ReactNode
 }
 
-const Labeled: FC<IProps> = ({ label, children }) => {
+const Labeled: FC<IProps> = ({ label, children, as = 'div' }) => {
+  const Element = as
+
   return (
-    <div className='flex w-full flex-col gap-1'>
+    <Element className='flex w-full flex-col gap-1'>
       <label className='text-ttDarkGray text-[12px] font-medium'>{label}</label>
       {children}
-    </div>
+    </Element>
   )
 }
 
