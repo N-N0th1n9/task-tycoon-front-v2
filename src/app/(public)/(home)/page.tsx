1bea59fdd39934a870f1bed487e7e2b825ece827
Nohop09 <ivan.gusev04@mail.ru>
2025-02-15 14:30:24 +0300

    feat: profile edit

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div className='flex flex-col items-center justify-center gap-20'>
       <h1>Home Page</h1>
-      <SelectInput />
+      {/* <SelectInput  /> */}
     </div>
   )
 }
--- src/features/update-profile-form/ui/index.tsx
+++ src/features/update-profile-form/ui/index.tsx
@@ -1,64 +1,108 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+
 import Button from '@src/shared/ui/button'
 import Input from '@src/shared/ui/input'
 import SwitchBar from '@src/shared/ui/switchbar'
 
 import CrossIcon from '../../../../public/assets/icons/cross'
 import SaveIcon from '../../../../public/assets/icons/save'
+import { useProfileStore } from '../model/stores/profile-info-store'
+
+type FormValues = {
+  email: string
+  firstName: string
+  lastName: string
+  nickname: string
+}
 
 const UpdateProfileDataForm = () => {
-  // const submitForm = () => {}
+  const { email, firstName, lastName, nickname, setProfile } = useProfileStore()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    setValue,
+  } = useForm<FormValues>({
+    defaultValues: { email, firstName, lastName, nickname },
+  })
+
+  useEffect(() => {
+    setValue('email', email)
+    setValue('firstName', firstName)
+    setValue('lastName', lastName)
+    setValue('nickname', nickname)
+  }, [email, firstName, lastName, nickname, setValue])
+
+  const onSubmit = (data: FormValues) => {
+    setProfile(data)
+    console.log(data)
+  }
 
   return (
-    <div className='flex w-full flex-col gap-9'>
-      <div className='flex w-full gap-4'>
-        <div className='flex w-full items-end gap-4'>
-          <Input
-            placeholder='example@mail.com'
-            className='w-full'
-            type='email'
-            label='Электронная почта'
-          />
-          <Button>Изменить</Button>
-        </div>
-      </div>
-      <div className='flex flex-col gap-6'>
-        <div className='flex w-full flex-col gap-4'>
-          <Input
-            placeholder='Иван'
-            className='w-full'
-            label='Имя'
-          />
-          <Input
-            placeholder='Иванов'
-            className='w-full'
-            label='Фамилия'
-          />
-          <Input
-            placeholder='Никнейм'
-            className='w-full'
-            label='Никнейм'
-          />
-          <div className='flex w-full flex-col'>
-            <SwitchBar label='Показываться статистику другим людям' />
-            <SwitchBar label='Показывать данные другим людям' />
-          </div>
-          <div className='flex justify-end gap-4'>
-            <Button
-              icon={<CrossIcon />}
-              disabled
-            >
-              Отменить
-            </Button>
-            <Button
-              icon={<SaveIcon />}
-              disabled
-            >
-              Сохранить
-            </Button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className='flex w-full flex-col gap-9'>
+        <div className='flex w-full gap-4'>
+          <div className='flex w-full items-end gap-4'>
+            <Input
+              placeholder='example@mail.com'
+              className='w-full'
+              type='email'
+              label='Электронная почта'
+              {...register('email', { required: 'Укажите ваш email' })}
+            />
+            <Button>Изменить</Button>
           </div>
         </div>
+        <div className='flex flex-col gap-6'>
+          <div className='flex w-full flex-col gap-4'>
+            <Input
+              placeholder='Иван'
+              className='w-full'
+              label='Имя'
+              type='firstName'
+              {...register('firstName', { required: 'Укажите ваше имя' })}
+            />
+            <Input
+              placeholder='Иванов'
+              className='w-full'
+              label='Фамилия'
+              type='lastName'
+              {...register('lastName', { required: 'Укажите ваше имя' })}
+            />
+            <Input
+              placeholder='Никнейм'
+              className='w-full'
+              label='Никнейм'
+              type='nickname'
+              {...register('nickname', { required: 'Укажите ваше имя' })}
+            />
+            <div className='flex w-full flex-col'>
+              <SwitchBar label='Показываться статистику другим людям' />
+              <SwitchBar label='Показывать данные другим людям' />
+            </div>
+            <div className='flex justify-end gap-4'>
+              <Button
+                icon={<CrossIcon />}
+                disabled
+              >
+                Отменить
+              </Button>
+              <Button
+                type='submit'
+                disabled={!isValid}
+                icon={<SaveIcon />}
+              >
+                Сохранить
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </form>
   )
 }
 
